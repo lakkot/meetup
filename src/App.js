760f910c25a59de594043b1c0f542356cb68ef2b
lakkot <40659615+lakkot@users.noreply.github.com>
2020-05-27 11:21:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  console.log(req.params.code)
+  
   return (
     <div className="App">
       <header className="App-header">
