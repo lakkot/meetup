e29538b5b21eb929954f4193af2bea18a0d53166
lakkot <40659615+lakkot@users.noreply.github.com>
2020-06-22 15:54:21 +0200

    Updates

--- precache-manifest.3e8f4e6ab574119b4a4dfbcc4eb986fa.js
+++ precache-manifest.3e8f4e6ab574119b4a4dfbcc4eb986fa.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7bed3fbd371e4e9d7b87be7ee8d491af",
+    "revision": "63a39ec2f78408cab171eb0a8491de10",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "7ecaba30b8bcce02d01e",
+    "revision": "9d02b17efb91664e5f0c",
     "url": "/meetup/static/css/main.fda6b431.chunk.css"
   },
   {
-    "revision": "4764afcf0ecb31ef76b0",
-    "url": "/meetup/static/js/2.58533ab5.chunk.js"
+    "revision": "331bc51fe98f261d23ad",
+    "url": "/meetup/static/js/2.fd9fa6c9.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/meetup/static/js/2.58533ab5.chunk.js.LICENSE.txt"
+    "url": "/meetup/static/js/2.fd9fa6c9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7ecaba30b8bcce02d01e",
-    "url": "/meetup/static/js/main.d55b0192.chunk.js"
+    "revision": "9d02b17efb91664e5f0c",
+    "url": "/meetup/static/js/main.2b919d1c.chunk.js"
   },
   {
     "revision": "b320879a2ac73b26d746",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.12725c98aca61c65007a2d4962e62bc7.js"
+  "/meetup/precache-manifest.3e8f4e6ab574119b4a4dfbcc4eb986fa.js"
 );
 
 self.addEventListener('message', (event) => {
